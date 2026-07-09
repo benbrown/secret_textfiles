57250eb35ce9573ee184fab192be482bf6ce16cd
Ben Brown <benbrown@gmail.com>
2026-07-09 10:34:24 -0500

    fix bug with query params

--- src/server.js
+++ src/server.js
@@ -450,7 +450,7 @@ app.get(`${ rootUrl }/feed`, async (req, res) => {
 
 app.get(`${ rootUrl }/read/*`, async (req, res) => {
   let post;
-  const pid = path.basename(req.url); //.replace(/\/read\//,'');
+  const pid = path.basename(req.path); //.replace(/\/read\//,'');
   try {
     const pathToFile = path.join(process.env.PATH_TO_TEXT,`${ pid }.txt`);
     debug('path to file', pathToFile);
@@ -480,7 +480,7 @@ app.get(`${ rootUrl }/read/*`, async (req, res) => {
 
 app.get(`${ rootUrl }/secret/edit/*`, auth, async (req, res) => {
   let post;
-  const pid = path.basename(req.url); // .replace(/\/secret\/edit\//,'');
+  const pid = path.basename(req.path); // .replace(/\/secret\/edit\//,'');
   try {
     const pathToFile = path.join(process.env.PATH_TO_TEXT,`${ pid }.txt`);
     debug('path to file', pathToFile);
@@ -506,7 +506,7 @@ app.get(`${ rootUrl }/secret/edit/*`, auth, async (req, res) => {
 
 app.get(`${ rootUrl }/secret/delete/*`, auth, async (req, res) => {
   let post;
-  const pid = path.basename(req.url);
+  const pid = path.basename(req.path);
   // .replace(/\/secret\/delete\//,'');
   try {
     const pathToFile = path.join(process.env.PATH_TO_TEXT,`${ pid }.txt`);
